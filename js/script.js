1cdd0ef168bae9ab460adc509282a44ef8fbb383
Marcin Gajewski <marcingajewski159@gmail.com>
2023-02-20 00:39:24 +0000

    JS function added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,52 @@
-console.log("Hello World");
 
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
-
-let EUR = "0.22";
-let USD = "0.23";
-let ISK = "33.0";
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let currency = currencyElement.value;
-    let amount = amountElement.value;
-    let result;
+const calculateResult = (amount, currency) => {
+    const EUR = "0.22";
+    const USD = "0.23";
+    const ISK = "33.0";
 
     switch (currency) {
         case "EUR":
-            result = amount * EUR;
-            break;
-        case "USD":
-            result = amount * USD;
-            break;
-        case "ISK":
-            result = amount * ISK;
-    }
+            return amount * EUR;
 
-    resultElement.innerText = result.toFixed(2);
-});
+        case "USD":
+            return amount * USD;
+
+        case "ISK":
+            return amount * ISK;
+    }
+};
+
+const updateResultText = (amount, result, currency) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerHTML = `${amount.toFixed(2)}PLN=${result.toFixed(2)}${currency} `
+};
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+
+    const amount = + amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency)
+
+    updateResultText(amount, result, currency)
+
+};
+
+const init = () => {
+
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+
+};
+
+{
+
+    init();
+
+}
